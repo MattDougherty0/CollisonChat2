424afa50ce09ff1861971cc55d4af69bb530fe89
MattDougherty0 <49167559+MattDougherty0@users.noreply.github.com>
2023-02-22 04:54:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import streamlit as st
 from gpt_index import GPTSimpleVectorIndex
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 # Load the index from disk
 index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
+# Set the API key as an environment variable
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 # Define the app layout
 st.title('GPT Search')
